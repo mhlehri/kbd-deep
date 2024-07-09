e657e35fef5783549baa202ec988f6104481f9a2
mhlehri <lehrimirza101@gmail.com>
2024-07-10 01:16:10 +0600

    add product card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
+import Featured from "../components/section/Featured";
 import { Hero } from "../components/section/Hero";
 import Service from "../components/section/Service";
 
 export default function Home() {
   return (
-    <main className="space-y-10">
+    <main className="space-y-24 mb-20">
       <Hero />
       <Service />
+      <Featured />
     </main>
   );
 }
