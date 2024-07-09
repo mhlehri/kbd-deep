f8af1ca6e1745eb2ac8c9952e13288dc19929467
mhlehri <lehrimirza101@gmail.com>
2024-07-10 00:08:18 +0600

    modify hero section

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -1,6 +1,4 @@
 import { useCallback, useEffect, useState } from "react";
-import slide2 from "../../../public/images/hero/2.jpeg";
-import slide3 from "../../../public/images/hero/3.jpeg";
 import slide4 from "/images/hero/4.jpg";
 import slide5 from "/images/hero/5.jpg";
 import slide1 from "/images/hero/6.png";
@@ -9,9 +7,9 @@ export const Hero = () => {
   const [currentSlider, setCurrentSlider] = useState(0);
 
   const carouselImages = [
-    { title: "Work Louder", des: "11", img: slide4 },
-    { title: "ePBT Creative Studio", des: "22", img: slide5 },
-    { title: "Theseus75", des: "22", img: slide1 },
+    { title: "Work Louder", des: "sx-311", img: slide4, offer: "20%" },
+    { title: "ePBT Creative Studio", des: "et-x22", img: slide5, offer: "4%" },
+    { title: "Theseus75", des: "sf-4le", img: slide1, offer: "9%" },
   ];
 
   const nextSlider = useCallback(
@@ -26,57 +24,87 @@ export const Hero = () => {
   useEffect(() => {
     const intervalId = setInterval(() => {
       nextSlider();
-    }, 3000);
+    }, 4000);
     return () => clearInterval(intervalId);
   }, [nextSlider]);
 
   return (
-    <div className="h-72 my-4 w-full transform overflow-hidden rounded-lg before:bg-black/50 sm:h-96 md:h-[540px] lg:gap-10">
-      {carouselImages.map((slide, index) => {
-        const { img, title, des } = slide;
-        return (
-          <div
-            className={`${
-              index === currentSlider
-                ? "visible opacity-100"
-                : "invisible opacity-0"
-            } absolute inset-0 duration-500 ease-linear`}
-            key={`index_${index}`}
-          >
-            <img
-              src={img}
-              width="1200"
-              height="600"
-              alt={title}
-              className={`h-full w-full object-cover duration-500 ease-linear ${
-                index === currentSlider ? "scale-100" : "scale-105"
-              }`}
-            />
-            <div className="absolute inset-0 flex flex-col bg-black/40 p-5 text-center text-white drop-shadow-lg">
-              <div className="mb-0 mt-auto rounded-md bg-white/40 max-w-sm p-3 backdrop-blur-3xl">
-                <div className="mb-3 overflow-hidden text-xl font-semibold lg:text-3xl">
-                  <h1
-                    className={`${
-                      index === currentSlider ? "" : "translate-y-12"
-                    } duration-500 ease-linear`}
-                  >
-                    {title}
-                  </h1>
-                </div>
-                <div className="overflow-hidden text-sm md:text-base lg:text-lg">
-                  <p
-                    className={`${
-                      index === currentSlider ? "" : "-translate-y-12"
-                    } duration-500 ease-linear`}
-                  >
-                    {des}
-                  </p>
+    <div className="flex my-4">
+      <div className="h-72 w-full transform overflow-hidden rounded-lg before:bg-black/50 sm:h-96 md:h-[540px] lg:gap-10">
+        {carouselImages.map((slide, index) => {
+          const { img, title, des, offer } = slide;
+          return (
+            <div
+              className={`${
+                index === currentSlider
+                  ? "visible opacity-100"
+                  : "invisible opacity-0"
+              } absolute inset-0 duration-500 ease-linear`}
+              key={`index_${index}`}
+            >
+              <img
+                src={img}
+                width="1200"
+                height="600"
+                alt={title}
+                className={`h-full w-full object-cover duration-500 ease-linear ${
+                  index === currentSlider ? "scale-100" : "scale-105"
+                }`}
+              />
+
+              <div className="absolute inset-0 flex flex-col bg-black/40 p-5 text-center text-white drop-shadow-lg">
+                <p
+                  className={`${
+                    index === currentSlider ? "" : "-translate-y-6"
+                  } duration-500 ease-linear bg-orange-200 mt-0 ml-auto rounded-md px-2 py-1 text-orange-700`}
+                >
+                  {offer} OFF
+                </p>
+                <div className="mb-0 mt-auto rounded-md bg-white/40 max-w-sm p-3 backdrop-blur-3xl">
+                  <div className="mb-1 overflow-hidden text-xl font-semibold lg:text-3xl">
+                    <h1
+                      className={`${
+                        index === currentSlider ? "" : "translate-y-12"
+                      } duration-500 ease-linear`}
+                    >
+                      {title}
+                    </h1>
+                  </div>
+                  <div className="overflow-hidden text-sm md:text-base lg:text-lg">
+                    <p
+                      className={`${
+                        index === currentSlider ? "" : "-translate-y-12"
+                      } duration-500 ease-linear`}
+                    >
+                      {des}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      <div className="flex flex-col items-center justify-end gap-3 pl-3">
+        {carouselImages.map((slide, index) => {
+          const { img, title } = slide;
+          return (
+            <img
+              key={index}
+              src={img}
+              width={1200}
+              height={600}
+              className={`h-6 w-10 bg-black/20 sm:h-8 md:h-12 md:w-20 ${
+                currentSlider === index
+                  ? "opacity-100 ring ring-zinc-500"
+                  : "opacity-60"
+              } box-content cursor-pointer rounded-md md:rounded-lg`}
+              alt={title}
+              onClick={() => setCurrentSlider(index)}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
