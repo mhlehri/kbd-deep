6a636c4638dbd351d2c051b97cedde7f3de009cf
mhlehri <lehrimirza101@gmail.com>
2024-07-13 13:16:27 +0600

    add reset cart items modal

--- src/components/table/CartTable.tsx
+++ src/components/table/CartTable.tsx
@@ -6,15 +6,28 @@ import {
   Table,
   TableBody,
   TableCell,
+  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "../ui/table";
 import Qty from "./Qty";
+import { toast } from "sonner";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { useState } from "react";
 
 export function CartTable() {
   const cart = useAppSelector((state) => state.cart.items);
   const dispatch = useAppDispatch();
+  const [open, setOpen] = useState(false);
 
   return (
     <Table className="rounded-lg overflow-hidden bg-zinc-50">
@@ -28,44 +41,77 @@ export function CartTable() {
           <TableHead className="text-right">Action</TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody>
-        {cart.length ? (
-          cart.map((item, i) => {
-            return (
-              <TableRow key={item.slug}>
-                <TableCell className="font-medium">{i + 1}</TableCell>
-                <TableCell>{item.name}</TableCell>
-                <TableCell>{item.price}</TableCell>
-                <TableCell>
-                  <Qty
-                    slug={item.slug!}
-                    quantity={item.quantity}
-                    stock={item.stock}
-                  />
-                </TableCell>
-                <TableCell>{item.price * item.quantity}</TableCell>
-                <TableCell className="flex justify-end">
-                  <Button
-                    onClick={() => {
-                      dispatch(removeItem(item.slug));
-                    }}
-                    variant="outline"
-                    className="text-red-500"
-                  >
-                    <Trash2 />
-                  </Button>
-                </TableCell>
-              </TableRow>
-            );
-          })
-        ) : (
+
+      {cart.length ? (
+        <>
+          <TableBody>
+            {cart.map((item, i) => {
+              return (
+                <TableRow key={item.slug}>
+                  <TableCell className="font-medium">{i + 1}</TableCell>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell>{item.price}</TableCell>
+                  <TableCell>
+                    <Qty
+                      slug={item.slug!}
+                      quantity={item.quantity}
+                      stock={item.stock}
+                    />
+                  </TableCell>
+                  <TableCell>{item.price * item.quantity}</TableCell>
+                  <TableCell className="flex justify-end">
+                    <Button
+                      onClick={() => {
+                        dispatch(removeItem(item.slug));
+                        toast.success("Item Remove successfully!");
+                      }}
+                      variant="outline"
+                      className="text-red-500"
+                    >
+                      <Trash2 />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell className="text-right" colSpan={6}>
+                <Dialog open={open} onOpenChange={setOpen}>
+                  <DialogTrigger asChild>
+                    <Button variant="destructive" className="">
+                      Reset cart
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogTitle>Are you sure to reset cart?</DialogTitle>
+                    <DialogDescription>
+                      you can't undone this after reset.
+                    </DialogDescription>
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button>Cancel</Button>
+                      </DialogClose>
+                      <Button variant="destructive" onClick={() => {}}>
+                        Sure
+                      </Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </>
+      ) : (
+        <TableBody>
           <TableRow>
             <TableCell className="text-center" colSpan={6}>
               No Cart added Yet
             </TableCell>
           </TableRow>
-        )}
-      </TableBody>
+        </TableBody>
+      )}
     </Table>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
-      <Toaster duration={1000} position="top-center" richColors closeButton />
+      <Toaster duration={1000} position="top-center" richColors />
     </Provider>
   </React.StrictMode>
 );
